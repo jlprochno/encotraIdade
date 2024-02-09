f30c618d5296254f577fdd7ba404891ab453e13e
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:55:10 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace encotraIdade;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, por gentileza digite seu Nome: ");
+            string name = Console.ReadLine();
+        Console.WriteLine("agora, digite o ano que você nasceu: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+        int age = 2024 - year;
+
+        Console.WriteLine(name + ", sua idade é aproximadamente: " + age + " anos.");
+        
+
+    }
+}
